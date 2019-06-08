7640060805de9887b58c780957714da387432354
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-08 04:20:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(deli)
   if deli.empty?
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     current_line = "The line is currently:"
     deli.each.with_index(1) do |person, i|
@@ -13,7 +13,7 @@ end
 
 def take_a_number(deli, name)
   deli << name
-  
+  puts "Welcome #{name}"
 end
 
 def now_serving(deli)
