6fd731bff9a78baa420932debf9b8e6dc326a908
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-08 04:21:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(deli, name)
   deli << name
-  puts "Welcome #{name}. You are number "
+  puts "Welcome #{name}. You are number #{deli.length} in line."
 end
 
 def now_serving(deli)
